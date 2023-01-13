544cf5ea6292ddddc364466e3fa5404b2f0824e4
Alexey Tukachev <alexey.tukachev@perfectart.com>
2023-01-13 19:45:58 +0300

    show

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -1,4 +1,4 @@
 export { For } from "./src/For";
 export { If, ElseIf, Else } from "./src/If";
-export { Show } from "./src/Show";
+export { Show, Await, useAsyncValue, useAsyncError } from "./src/Show";
 export { Switch, Case, Default } from "./src/Switch";
--- packages/ui/src/Show/Switch.stories.tsx
+++ packages/ui/src/Show/Switch.stories.tsx
@@ -1,8 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import type { Meta, StoryFn } from "@storybook/react";
-import React from "react";
+import React, { Suspense, useReducer, useState } from "react";
 import { If } from "../If";
 
-import { Show } from "./Show";
+import { Show, Await, useAsyncValue } from "./Show";
 
 const meta = {
   title: "UI/Show",
@@ -18,13 +19,20 @@ export const WithFallback: StoryFn<typeof Show> = () => {
       resolve({ value: "Value!" });
     }, 1000);
   });
+  const [_, reload] = useReducer(
+    (s) => s + 1,
+    () => 0,
+  );
 
   return (
-    <Show when={promise} fallback={<div>Loading...</div>}>
-      {(res) => {
-        return <div>{res.value}</div>;
-      }}
-    </Show>
+    <>
+      <button onClick={reload}>Reload</button>
+      <Show when={promise} fallback={<div>Loading...</div>}>
+        {(res) => {
+          return <div>{res.value}</div>;
+        }}
+      </Show>
+    </>
   );
 };
 
@@ -34,22 +42,29 @@ export const WithError: StoryFn<typeof Show> = () => {
       reject(new Error("Promise was rejected"));
     }, 1000);
   });
+  const [_, reload] = useReducer(
+    (s) => s + 1,
+    () => 0,
+  );
 
   return (
-    <Show
-      when={promise}
-      fallback={<div>Loading...</div>}
-      errorElement={(e) => (
-        <div>
-          Some error was occured 🙁{" "}
-          {e instanceof Error && `(cause: "${e.message}")`}
-        </div>
-      )}
-    >
-      {(res) => {
-        return <div>{res.value}</div>;
-      }}
-    </Show>
+    <>
+      <button onClick={reload}>Reload</button>
+      <Show
+        when={promise}
+        fallback={<div>Loading...</div>}
+        errorElement={(e) => (
+          <div>
+            Some error was occured 🙁{" "}
+            {e instanceof Error && `(cause: "${e.message}")`}
+          </div>
+        )}
+      >
+        {(res) => {
+          return <div>{res.value}</div>;
+        }}
+      </Show>
+    </>
   );
 };
 
@@ -74,16 +89,52 @@ export const WithFalsableValue: StoryFn<typeof Show> = () => {
       }
     }, 1000);
   });
+  const [_, reload] = useReducer(
+    (s) => s + 1,
+    () => 0,
+  );
 
   return (
-    <Show when={promise} fallback={<div>Loading...</div>}>
-      {(res) => (
-        <If
-          test={res}
-          then={({ value }) => <div>{value}</div>}
-          else={<div>No value</div>}
-        />
-      )}
-    </Show>
+    <>
+      <button onClick={reload}>Reload</button>
+      <Show when={promise} fallback={<div>Loading...</div>}>
+        {(res) => (
+          <If
+            test={res}
+            then={({ value }) => <div>{value}</div>}
+            else={<div>No value</div>}
+          />
+        )}
+      </Show>
+    </>
   );
 };
+
+export const WithAwait: StoryFn<typeof Show> = () => {
+  const promise = new Promise<{ value: string }>((resolve) => {
+    return setTimeout(() => {
+      resolve({ value: "Value!" });
+    }, 1000);
+  });
+  const [_, reload] = useReducer(
+    (s) => s + 1,
+    () => 0,
+  );
+
+  return (
+    <>
+      <button onClick={reload}>Reload</button>
+      <Suspense fallback={<div>Fallback...</div>}>
+        <Await resolve={promise}>
+          <ChildComponent />
+          <ChildComponent />
+        </Await>
+      </Suspense>
+    </>
+  );
+};
+
+function ChildComponent() {
+  const data = useAsyncValue();
+  return <div>{data?.value}</div>;
+}
